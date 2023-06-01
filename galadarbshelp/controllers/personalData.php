129277dad9dce173a662737f2a907ae0778c5f04
HighRollerLV <ip19.r.dambitis@vtdt.edu.lv>
2023-06-01 21:59:40 +0300

    Account Profile Countries settings(No Error Message)

--- galadarbshelp/controllers/personalData.php
+++ galadarbshelp/controllers/personalData.php
@@ -20,8 +20,6 @@ if (empty($firstName) || empty($lastName) || empty($location) || empty($age)) {
     $insertMsg = "All fields must be complete";
 } else if ($age < 18) {
     $insertMsg = "You have to be 18 years old or older!";
-} else if (!in_array($location, $countries)) {
-    $insertMsg = "Invalid location selected";
 } else {
     $stmt = $conn->prepare("UPDATE loginhelp SET firstName=?, lastName=?, location=?, age=? WHERE id=?");
     $stmt->bind_param("ssssi", $firstName, $lastName, $location, $age, $userID);
